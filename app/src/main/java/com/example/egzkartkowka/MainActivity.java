8948c2278d917cc9e1dd5e26eeabd41ddf2e6807
Kosmatheo <Kosmafzar@gmail.com>
2026-01-16 08:35:12 +0100

    Na5

--- app/src/main/java/com/example/egzkartkowka/MainActivity.java
+++ app/src/main/java/com/example/egzkartkowka/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.egzkartkowka;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -48,6 +52,19 @@ public class MainActivity extends AppCompatActivity {
                     rzeczyDoZrobieniaArrayList.add(rzecz);
                     arrayAdapter.notifyDataSetChanged();
                     editTextRzeczDoZrobienia.setText("");
+                }
+            }
+    );
+    listView.setOnItemClickListener(
+            new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    /*view.setBackgroundColor(Color.GRAY);
+                    TextView textView = (TextView) view;
+                    textView.setTextColor(Color.WHITE);
+                    textView.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);*/
+                    rzeczyDoZrobieniaArrayList.remove(i);
+                    arrayAdapter.notifyDataSetChanged();
                 }
             }
     );
